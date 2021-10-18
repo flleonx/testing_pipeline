ca5e32b9c697a233209bf12a7ae5ae25d8482e39
flleonx <69682697+flleonx@users.noreply.github.com>
2021-10-18 00:42:53 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "====== BUILDING THE IMAGE ====="
-docker build -t testing_pipeline:1.0 .
-echo "====== RUNNING THE CONTAINER ====="
-docker run --name testing_container -p 3000:3000 -d testing_pipeline:1.0
-sleep 2s
-echo "====== STARTING TESTS ====="
-npm test
-docker kill testing_container
-docker rm testing_container
